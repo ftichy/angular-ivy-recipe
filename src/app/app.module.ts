38d7a3d8b5b3dcd5789a1b920b99c753aaccad5f
ftichy <59052141+ftichy@users.noreply.github.com>
2022-11-17 14:01:58 +0100

    9 files modified, 15 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { SharedComponent } from './shared/shared.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    SharedComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
     ShoppingListComponent,
+    ShoppingEditComponent,
+    DropdownDirective
   ],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
